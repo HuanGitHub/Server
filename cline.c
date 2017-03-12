f716879eec7574059272438ba4623ac6145b3522
zhanghuan <601335832@qq.com>
2017-03-13 07:19:30 +0800

    have return IP addr

--- cline.c
+++ cline.c
@@ -6,7 +6,6 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-
 int main()
 {
 	int se_socket;
@@ -15,21 +14,18 @@ int main()
 	se_addr.sin_port=htons(8080);
 	se_addr.sin_addr.s_addr=0;
 	se_socket=socket(AF_INET,SOCK_STREAM,0);
-
 	int len=sizeof(se_addr);
 	printf("%d\n",len);
 	int con=connect(se_socket,(void*)&se_addr,len);
 	printf("%d\n",con);
 	char *str="hello world";            //output string
 	char buf[]="test";
+	scanf("%s",buf);
 	write(se_socket,buf,sizeof(buf));
 	/*char buf[100];
 	read(se_socket,buf,100);
 	printf("%s\n",buf);
 */
-
-
-
 	close(se_socket);
 }	
 
--- server.c
+++ server.c
@@ -1,69 +1,71 @@
+#include <sys/types.h>
 #include <stdio.h>
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <netinet/ip.h>
+#include <netinet/in.h>
 #include <unistd.h>
+#include <string.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
+#include <pthread.h>
+void *pthread_write(void *ad);
+void *pthread_read(void *fd);
 int main()
 {
-	int se_socket,cl_socket;
-	struct sockaddr_in se_addr,cl_addr;	
-	se_addr.sin_family=AF_INET;
-	se_addr.sin_port=htons(8080);
-	se_addr.sin_addr.s_addr=0;
-	se_socket=socket(AF_INET,SOCK_STREAM,0);
-	if(se_socket<0)
+	int sockfd,nfd;
+	pthread_t p_r,p_w;
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd==-1)
 	{
-		printf("socket fail");
-		exit(0);
+		printf("creat socket fail");
+		return 1;
 	}
-	if(bind(se_socket,(void*)&se_addr,sizeof(se_addr))==-1)
+	struct sockaddr_in addr,naddr;
+	addr.sin_family=AF_INET;
+	addr.sin_port=htons(8080);
+	addr.sin_addr.s_addr=inet_addr("192.168.120.117");
+	if((bind(sockfd,(void*)&addr,sizeof(addr))==-1))
 	{
-		printf("bind fail");
-		exit(0);
+		printf("creat bind fail");
+		return 1; 
 	}
-	listen(se_socket,5);
-	char buf[110];
-	int len=sizeof(cl_addr);
+	int stl,l=0,i=0;
+	listen(sockfd,5);
+	char ip_addr[1];
+	char *ad[5]={0};
 	while(1)
 	{
-		cl_socket=accept(se_socket,(void*)&cl_addr,&len);
-//		cl_socket=accept(se_socket,NULL,NULL);
-		 int r=read(cl_socket,buf,sizeof(buf));
-    	 printf("%d\n%d\n",r,cl_socket);
-	   	printf("%s\n",buf);
- 		write(cl_socket,buf,110);
-		
-	}
-	close(cl_socket);
-	close(se_socket);
+		nfd=accept(sockfd,(void*)&naddr,&stl);		
+		char *k=inet_ntoa(naddr.sin_addr);
+		printf("the client IP:  %s\n",k);
+		pthread_create(&p_r,NULL,pthread_read,&nfd);	
+		pthread_create(&p_w,NULL,pthread_write,&nfd);
+		for(i=0;k!=ad[i]&&i<5;i++)
+{		
+			ad[i]=k;
+			printf("the ad[%d] value: %s\n",i,ad[i]);			
+		break;
+}
+}
+		printf("l the valte:%d\n",l);
+
+	close(nfd);
+	close(sockfd);
 
 }
+void *pthread_read(void *ad)
+{	
+	char buf[100];	
+	int td=*(int *)ad;
+	read(td,buf,sizeof(buf));
+	printf("%s\n",buf);
+	close(td);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+void *pthread_write(void *ad)
+{
+	char buf[100];
+	int td=*(int *)ad;
+	write(td,"zhang",sizeof(buf));
+	printf("%s\n",buf);
+	close(td);
+}
